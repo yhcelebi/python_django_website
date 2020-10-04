6c595fce8a1fae8af471bd076089684984cc626d
Hamza Çelebi <yhcelebi@gmail.com>
2020-10-04 19:19:58 +0300

    done

---
+++ django_project/blog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserListView
+from users import views as user_views
+
+urlpatterns = [
+    path("", PostListView.as_view(), name="home"),
+    path("user/<username>", UserListView.as_view(), name="user-posts"),
+    path("about/", views.about, name="about"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete", PostDeleteView.as_view(), name="post-delete"),
+]    
